c694db7a8cfd16965bfc785416c937c0d2a2e5a6
justincschoi <justincschoi@gmail.com>
2023-08-25 17:59:00 -0400

    Create operate function to evaluate equation

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const numberButtons = document.querySelectorAll('.number');
 const operatorButtons = document.querySelectorAll('.operator');
+const equals = document.querySelector('.equals');
 const displayTop = document.querySelector('.display-top');
 const displayBot = document.querySelector('.display-bot');
 
 let firstNumber = '';
 let secondNumber = '';
+let total = 0;
 let operator = '';
 
 operatorButtons.forEach(operatorButton => {
@@ -28,11 +30,48 @@ numberButtons.forEach(numberButton => {
     })
 })
 
+equals.addEventListener('click', operate);
+
+function operate() {
+    if (operator == '+') {
+        add();
+        updateDisplayTop();
+    }
+    else if (operator == '-') {
+        subtract();
+        updateDisplayTop();
+    }
+    else if (operator == '*') {
+        multiply();
+        updateDisplayTop();
+    }
+    else if (operator == '/') {
+        divide();
+        updateDisplayTop();
+    }
+
+};
+
+function add() {
+    total = parseFloat(firstNumber) + parseFloat(secondNumber);
+};
+
+function subtract() {
+    total = parseFloat(firstNumber) - parseFloat(secondNumber);
+}
+
+function multiply() {
+    total = parseFloat(firstNumber) * parseFloat(secondNumber);
+}
+
+function divide() {
+    total = parseFloat(firstNumber) / parseFloat(secondNumber);
+}
+
 function updateDisplayTop() {
-    displayTop.innerText = firstNumber + operator + secondNumber;
+    displayTop.innerText = " = " + total;
 };
 
 function updateDisplayBot() {
     displayBot.innerText = firstNumber + operator + secondNumber;
-
-}
+};
